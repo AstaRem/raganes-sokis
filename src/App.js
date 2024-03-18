13c22b312cb21bc5475ef4122b7c9b576b6244c8
Asta Rem <asta.rem@hotmail.com>
2024-03-18 21:34:13 +0000

    create initial components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigacija from './components/Navigacija';
+// index.js or App.js
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+          <header className="App-header row text-primary-emphasis" >
+            <div className="col-2">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+
+            <div className="col-10">
+               <Navigacija />
+            </div>
+          </header>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Navigacija.js
@@ -0,0 +1,45 @@
+
+function Navigacija (){
+    return(
+        <>
+        <nav>
+            <ul className='navigation-list nav justify-content-center ' >
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Pradzia</a>
+                </li>
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Botaniniai kvepalai</a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Renginiai</a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Krautuve</a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Imonems</a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Blogas</a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Apie mane </a>
+                </li>
+
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Kontaktai</a>
+                </li>
+
+
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Navigacija;
