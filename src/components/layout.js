ec4a06b6715aa4915f6427fc5830b46896657ec3
Asta Rem <asta.rem@hotmail.com>
2024-03-31 21:06:59 +0100

    add layout.css some color, working

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet"
+import * as styles from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <div className = {`${styles.mainBackground} ${styles.layoutText}`} >
         <Helmet>
             <title>{pageTitle}</title>
         </Helmet>
+        {/* <p className={styles.layoutText}>some text here</p> */}
       <nav>
-        <ul>
-          <li><Link to="/">Pradzia</Link></li>
+        <ul >
+          <li ><Link to="/">Pradzia</Link></li>
           <li><Link to="/Botaniniai">Botaniniai kvepalai</Link></li>
           <li><Link to="/Renginiai">Renginiai</Link></li>
           <li><Link to="/Krautuve">Krautuve</Link></li>
